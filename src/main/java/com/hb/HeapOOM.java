ee3a0bcf85eff540bb0d837487e02a3681081c46
hubing <hb_ccup@126.com>
2021-09-14 11:37:55 +0800

    增加add

--- src/main/java/com/hb/HeapOOM.java
+++ src/main/java/com/hb/HeapOOM.java
@@ -14,7 +14,8 @@ public class HeapOOM {
     public static void main(String[] args) {
         ArrayList<OOMObject> list = new ArrayList<>();
         while (true){
-
+                list.add(new OOMObject());
         }
     }
 }
+
